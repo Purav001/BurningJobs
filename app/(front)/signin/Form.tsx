3895bbb577055a0aac02eeb777a033f9f4e119f6
Purav001 <puravsaluja4@gmail.com>
2024-07-04 16:07:13 +0530

    signin form

--- app/(front)/signin/Form.tsx
+++ app/(front)/signin/Form.tsx
@@ -1,8 +1,14 @@
+'use client'
 import { signIn, useSession } from "next-auth/react"
 import { useSearchParams } from "next/navigation"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
+import Link from 'next/link'
+import Lottie from 'react-lottie'
+import animationData from '@/components/loading.json'
+
+
 
 type Inputs = {
     email: String
@@ -15,6 +21,15 @@ const Form = () => {
     const params = useSearchParams()
     let callbackUrl= params.get('callbackUrl') || '/'
     const router = useRouter()
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    }
 
     const{
         register,
@@ -39,7 +54,89 @@ const Form = () => {
         })
     }
     return (
-        <div></div>
+        <div>
+            {session && session.user && (
+                <div className="h-full flex justify-center items-center w-full">
+                <Lottie options={defaultOptions} height={200} width={200} />
+                </div>
+            )}
+            <div className="max-w-sm  mx-auto card bg-base-300 my-4">
+                {!(session && session.user) && (
+                <div className="card-body">
+                    <h1 className="card-title">Sign in</h1>
+                    {params.get('error') && (
+                    <div className="alert text-error">
+                        {params.get('error') === 'CredentialsSignin'
+                        ? 'Invalid email or password'
+                        : params.get('error')}
+                    </div>
+                    )}
+                    {params.get('success') && (
+                    <div className="alert text-success">{params.get('success')}</div>
+                    )}
+                    <form onSubmit={handleSubmit(formSubmit)}>
+                    <div className="my-2">
+                        <label className="label" htmlFor="email">
+                        Email
+                        </label>
+                        <input
+                        type="text"
+                        id="email"
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                            message: 'Email is invalid',
+                            },
+                        })}
+                        className="input input-bordered w-full max-w-sm"
+                        />
+                        {errors.email?.message && (
+                        <div className="text-error">{errors.email.message}</div>
+                        )}
+                    </div>
+                    <div className="my-2">
+                        <label className="label" htmlFor="password">
+                        Password
+                        </label>
+                        <input
+                        type="password"
+                        id="password"
+                        {...register('password', {
+                            required: 'Password is required',
+                        })}
+                        className="input input-bordered w-full max-w-sm"
+                        />
+                        {errors.password?.message && (
+                        <div className="text-error">{errors.password.message}</div>
+                        )}
+                    </div>
+                    <div className="my-4">
+                        <button
+                        type="submit"
+                        disabled={isSubmitting}
+                        className="btn btn-primary w-full"
+                        >
+                        {isSubmitting && (
+                            <span className="loading loading-spinner"></span>
+                        )}
+                        Sign in
+                        </button>
+                    </div>
+                    </form>
+                    <div>
+                    Need an account?{' '}
+                    <Link
+                        className="link"
+                        href={`/register?callbackUrl=${callbackUrl}`}
+                    >
+                        Register
+                    </Link>
+                    </div>
+                </div>
+                )}
+            </div>
+        </div>
     )
 }
 export default Form
--- app/(front)/signin/page.tsx
+++ app/(front)/signin/page.tsx
@@ -2,10 +2,10 @@ import { Metadata } from 'next'
 import Form from './Form'
 
 export const metadata: Metadata = {
-  title: `Sign in - Burning Jobs`,
-  description: `Welcome Back! Sign In to Your Burning Jobs Account.`,
+    title: `Sign in - Burning Jobs`,
+    description: `Welcome Back! Sign In to Your Burning Jobs Account.`,
 }
 
 export default async function Signin() {
-  return <Form />
+    return <Form />
 }
